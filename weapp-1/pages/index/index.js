6a6dc910fc211d4b0175cdfeb45da5f1c9324e89
sunkun <sk@360shop.cn>
2018-05-22 18:28:35 +0800

    2018-5-22 18:28:30

--- weapp-1/pages/index/index.js
+++ weapp-1/pages/index/index.js
@@ -7,7 +7,7 @@ Page({
         autoplay: true,
         interval: 3000,
         duration: 1000,
-        loadingHidden: false  // loading
+        loadingHidden: true,  // loading
     },
 
     //事件处理函数
@@ -25,61 +25,5 @@ Page({
                 userInfo: userInfo
             })
         })
-
-        //sliderList
-        wx.request({
-            url: 'http://huanqiuxiaozhen.com/wemall/slider/list',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                that.setData({
-                    images: res.data
-                })
-            }
-        })
-
-        //venuesList
-        wx.request({
-            url: 'http://huanqiuxiaozhen.com/wemall/venues/venuesList',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                that.setData({
-                    venuesItems: res.data.data
-                })
-                setTimeout(function () {
-                    that.setData({
-                        loadingHidden: true
-                    })
-                }, 1500)
-            }
-        })
-
-        //choiceList
-        wx.request({
-            url: 'http://huanqiuxiaozhen.com/wemall/goods/choiceList',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                that.setData({
-                    choiceItems: res.data.data.dataList
-                })
-                setTimeout(function () {
-                    that.setData({
-                        loadingHidden: true
-                    })
-                }, 1500)
-            }
-        })
-
     }
 })
