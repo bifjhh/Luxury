845234c3de70008585cc2dc84f9e90212d0a8a52
sunkun <sk@360shop.cn>
2018-07-25 14:12:59 +0800

    分类完成

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -1,30 +1,15 @@
 var app = getApp()
 Page({
 	data: {
-		navLeftItems: [],
-		navRightItems: [],
-		curNav: 0,
-		curIndex: 0,
+		brandList: [],
+		nav: [],
+		goodsList: [],
 		is_show: 1,
+		curNav:'',
 	},
 	onLoad: function () {
-		let that = this
+		var that = this
 
-
-	},
-	onShow() {
-		let that = this
-
-		wx.pro.request({
-			url: app.globalData.uri + 'Index/brandList',
-			data: data,
-			method: "POST",
-			header: {
-				'content-type': 'application/x-www-form-urlencoded'
-			}
-		}).then(res => {
-			console.log(res)
-		})
 	},
 	toDetail() {
 		wx.navigateTo({
@@ -34,19 +19,73 @@ Page({
 
 	//事件处理函数
 	switchRightTab: function (e) {
-		let id = e.target.dataset.id,
-			index = parseInt(e.target.dataset.index);
-		this.setData({
-			curNav: id,
-			curIndex: index
-		})
+		let that = this;
+		let id = e.target.dataset.id;
+		that.setData({curNav:id})
+
+		that.getGoods(that, id)
 	},
 	show(e) {
-		let that = this;
-		let is_show = e.target.dataset.show * 1;
+		var that = this;
+		var is_show = e.target.dataset.show * 1;
 		that.setData({
 			is_show
 		})
-	}
+	},
+	onLoad: function (options) {
+		let that = this
+		wx.pro.request({
+			url: app.globalData.uri + 'Index/brandList',
+			data: {},
+			method: "POST",
+			header: {
+				'content-type': 'application/x-www-form-urlencoded'
+			}
+		}).then(res => {
+			that.setData({
+				brandList: res.data.data
+			})
+			console.log(res.data.data)
+		})
 
+		wx.pro.request({
+			url: app.globalData.uri + 'Index/cateList',
+			data: {
+				pid: 0
+			},
+			method: "POST",
+			header: {
+				'content-type': 'application/x-www-form-urlencoded'
+			}
+		}).then(res => {
+			that.setData({
+				nav: res.data.data
+			})
+			that.getGoods(that, res.data.data[0].cate_id)
+		})
+
+
+	},
+
+	getGoods(that, id) {
+		wx.pro.request({
+			url: app.globalData.uri + 'Index/cateList',
+			data: {
+				pid: id
+			},
+			method: "POST",
+			header: {
+				'content-type': 'application/x-www-form-urlencoded'
+			}
+		}).then(res => {
+			that.setData({
+				goodsList: res.data.data
+			})
+		})
+	},
+	toDetail() {
+		wx.navigateTo({
+			url: '/pages/shop/goods/goods',
+		})
+	},
 })
